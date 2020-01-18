a8739c583502c409e9c66d2cd86d153b984d7ea4
unknown <giovanijfc@gmail.com>
2020-01-18 18:13:59 -0300

    Adds auto verification in commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ const restify = require('restify');
 
 const PORT = 5000;
 const app = restify.createServer();
-app.get('/', (req, res) => {
-  console.log('teste');
-});
+app.get('/', (req, res) => res.send('localhost:5000'));
 app.listen(PORT);
