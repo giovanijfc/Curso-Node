43c770a820252a7e7ea112c521310e875df75e27
giovanijfc <giovanijfc@gmail.com>
2020-01-18 21:16:30 -0300

    Adds create connection with database

--- src/routes/noticias.js
+++ src/routes/noticias.js
@@ -1,3 +1,18 @@
+const mysql = require('mysql');
+
 module.exports = app => {
-  app.get('/noticias', (req, res) => res.render('noticias/noticias'));
+  app.get('/noticias', async (req, res) => {
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '1234',
+      database: 'portal_noticias'
+    });
+
+    connection.query('select * from noticias', (error, result) => {
+      res.send(result);
+    });
+
+    connection.end();
+  });
 };
